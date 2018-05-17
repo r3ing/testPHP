c524b4d99bf7c9faef54f3f8774db1e0cda01d1b
Rey <reirpinf@gmail.com>
2018-05-17 19:07:59 -0400

    test index.html

--- index.php
+++ index.php
@@ -22,17 +22,20 @@ $ldapport = 8080;                 // your ldap server's port number
 $ldaprdn  = 'Andres Gutierrez Valle (CL)';     // ldap rdn or dn
 $ldappass = 'Amparo16';  // associated password
 
+//$server = "ldaps://w1156p.ripley.local";
+$server = "10.0.1.50";
+
 // connect to ldap server
-$ldapconn = ldap_connect($ldaphost, $ldapport);
+$ldapconn = ldap_connect($server);
 
 if ($ldapconn) {
 
     echo "Conecting ......";
-
-    echo "Connected to the server...Conect result: ".$ldapconn;
-
+    echo "<br>";
+    echo "Connected to the server...Conect result: ".$server;
+    echo "<br>";
     // binding to ldap server
-    $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
+    $ldapbind = ldap_bind($server);
 
     // verify binding
     if ($ldapbind) {
@@ -42,6 +45,7 @@ if ($ldapconn) {
     }
 
 }else{
+    echo "<br>";
     echo "Could not connect to LDAP server.";
 }
 
